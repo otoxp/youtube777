bb6dc6dd14ce82785a02e8b2f69e2ac33a1c0eb4
casa <carloamorim@gmail.com>
2025-04-24 11:41:41 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icons/logo-192.png',
+        '/icons/logo-512.png',
+        '/styles.css',
+        '/scripts.js',  // Adicione outros arquivos que deseja cachear
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = ['my-cache'];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
